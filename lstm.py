9e45adabcafab20485c9305c878d6ee6fa0b8410
sandermn <sandermagner@gmail.com>
2020-10-28 10:51:09 +0100

    Added file
    Added lstm.py

---
+++ lstm.py
@@ -0,0 +1,8 @@
+import numpy
+from keras.datasets import imdb
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+
