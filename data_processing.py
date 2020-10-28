a67cd315029952e42d966d0bdd46f62c9f9c9821
Haakon <haakon.lyngstad@gmail.com>
2020-10-28 10:39:16 +0100

    Created data import

--- data_processing.py
+++ data_processing.py
@@ -2,8 +2,24 @@ from sklearn import model_selection, preprocessing, linear_model, naive_bayes, m
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn import decomposition, ensemble
 
-import pandas, xgboost, numpy, textblob, string
+import pandas, xgboost, numpy, textblob, string, csv
 from keras.preprocessing import text, sequence
 from keras import layers, models, optimizers
 
-print(10)
+def import_dataset(filename):
+    file = open(filename, encoding="utf-8")
+    data = list(csv.reader(file, skipinitialspace=True))
+    col_list = ["title", "text"]
+    df = pandas.read_csv(filename, usecols=col_list, encoding="utf-8")
+    return df
+    
+
+def get_dataframe():
+    trueDf = import_dataset('True.csv')
+    trueDf['class'] = 1
+    fakeDf = import_dataset('Fake.csv')
+    fakeDf['class'] = 0
+
+    return pandas.concat([trueDf, fakeDf])
+
+print(get_dataframe())
