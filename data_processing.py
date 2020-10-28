fafbd2c2dc82f43cfd1db2f560e32d3a0fd590b4
Haakon <haakon.lyngstad@gmail.com>
2020-10-28 09:42:24 +0100

    Added requirements.txt

---
+++ data_processing.py
@@ -0,0 +1,8 @@
+from sklearn import model_selection, preprocessing, linear_model, naive_bayes, metrics, svm
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn import decomposition, ensemble
+
+import pandas, xgboost, numpy, textblob, string
+from keras.preprocessing import text, sequence
+from keras import layers, models, optimizers
+
