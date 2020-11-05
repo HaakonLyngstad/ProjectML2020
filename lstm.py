060243c7e92801a899fe13e1264e7656b8372569
sandermn <sandermagner@gmail.com>
2020-11-05 14:43:00 +0100

    Added dataset & predict
    Added a new dataset for combined news and DJIA

--- lstm.py
+++ lstm.py
@@ -1,4 +1,5 @@
 import numpy
+import keras.layers as layers
 from keras.datasets import imdb
 from keras.models import Sequential
 from keras.layers import Dense
@@ -6,3 +7,8 @@ from keras.layers import LSTM
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 
+# setup
+input_size = 100
+
+
+input_layer = layers.Input((input_size,))
