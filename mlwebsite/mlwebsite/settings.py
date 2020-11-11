ba8c4b58d5e4befae59d25d6d4c711ee5f375291
Nicolai Lysaa <nicolailysaa@gmail.com>
2020-11-11 13:14:51 +0100

    Hide secret key from git

--- mlwebsite/mlwebsite/settings.py
+++ mlwebsite/mlwebsite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '41uyl#60d!!39dx16e6ms_vb)73($_0io(igf4ca)-uw_87o^%'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
