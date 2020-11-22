068df02a46dc76d116044d97c210734157179982
Haakon <haakon.lyngstad@gmail.com>
2020-11-22 18:02:36 +0100

    Parallax bagging fix

--- mlwebsite/mlwebsite/urls.py
+++ mlwebsite/mlwebsite/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-    path('api-auth/', include('rest_framework.urls')),
 ]
 
 if settings.DEBUG:
