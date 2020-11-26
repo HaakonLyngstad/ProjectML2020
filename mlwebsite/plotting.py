dc2ac5d89084f3f7cce2257b2123cec746c96a6e
sandermn <sandermagner@gmail.com>
2020-11-26 22:39:13 +0100

    update plotting

--- mlwebsite/plotting.py
+++ mlwebsite/plotting.py
@@ -7,6 +7,9 @@ df = pd.read_csv('models/stacking_metrics.csv')
 #df1.plot(x='Classifier', y='Precision', kind='bar')
 #df1.plot(x='Classifier', y='Recall', kind='bar')
 
-df.plot.bar(x='Classifier', rot=0, title='Classifiers', figsize=(20, 10), fontsize=14)
+plot = df.plot.bar(x='Classifier', rot=0, title='Classifiers', figsize=(20, 10), fontsize=14)
+params = {'legend.fontsize': 20,
+          'legend.handlelength': 2}
+plt.rcParams.update(params)
 
 plt.show()
